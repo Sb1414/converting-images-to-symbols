be0e606bc11cdb8b1415c9053beb0ef3d69f9642
Sb1414 <kalkamanova.sabina14@gmail.com>
2022-11-27 21:15:35 +0300

    convert to b/w

---
+++ convertImages/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertImages
+{
+    internal static class Extensions
+    {
+        public static void ToCrayscale(this Bitmap bitmap) // конвертация в ч/б
+        {
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    int avg = (pixel.R + pixel.G + pixel.B) / 3;
+                    bitmap.SetPixel(x, y, Color.FromArgb(pixel.A, avg, avg, avg));
+                }
+            }
+        }
+    }
+}
